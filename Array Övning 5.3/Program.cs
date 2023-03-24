93afb5929e535ccdc3541257945f2c505cd4cb6e
Anton <>
2023-03-24 09:21:26 +0100

    Add project files.

---
+++ Array Övning 5.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Tal = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int StörstaTal = 0;
+
+            for(int i = 0; i <Tal.Length; i++)
+            {
+                Console.WriteLine(Tal[i]);
+                if (Tal[i]>StörstaTal)
+                {
+                    StörstaTal = Tal[i];
+                }
+
+            }
+
+            Console.WriteLine("Det största talet är " + StörstaTal);
+
+
+        }
+    }
+}
